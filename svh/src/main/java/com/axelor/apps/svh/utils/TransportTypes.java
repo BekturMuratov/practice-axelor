5e2fce6235b017313a9af19e031d909bd3518a68
BekturMuratov <Muratov225@gmail.com>
2026-01-20 15:31:51 +0600

    some things is changed and some recommendation from Nurzatbek is done

--- svh/src/main/java/com/axelor/apps/svh/utils/TransportTypes.java
+++ svh/src/main/java/com/axelor/apps/svh/utils/TransportTypes.java
@@ -2,11 +2,11 @@ package com.axelor.apps.svh.utils;
 
 public final class TransportTypes {
 
-  private TransportTypes() {} // запрет на создание экземпляра
+  private TransportTypes() {}                                                                                           // запрет на создание экземпляра
 
-  public static final String FREIGHT = "FREIGHT"; // Грузовое АТС
-  public static final String SPECIAL = "SPECIAL"; // Спецтехника
-  public static final String CAR_CARRIER = "CAR_CARRIER"; // Автовоз
-  public static final String PASSENGER = "PASSENGER"; // Легковой транспорт
-  public static final String COMPANY_CAR = "COMPANY_CAR"; // Служебное авто
+  public static final String FREIGHT = "FREIGHT";                                                                       // Грузовое АТС
+  public static final String SPECIAL = "SPECIAL";                                                                       // Спецтехника
+  public static final String CAR_CARRIER = "CAR_CARRIER";                                                               // Автовоз
+  public static final String PASSENGER = "PASSENGER";                                                                   // Легковой транспорт
+  public static final String COMPANY_CAR = "COMPANY_CAR";                                                               // Служебное авто
 }
